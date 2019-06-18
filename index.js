65ccbe95ec70001652a5577e326e5a447994e41d
joel jimenez <joel.jimenez@mici.gob.pa>
2019-06-18 17:15:06 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+'use strict'
+const path = require('path');
+const bodyP = require('body-parser');
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+
+// Enabling JSON bodyParser
+app.use(bodyP());
+
+// Archivos estáticos
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('base.pug');
+  });
+
+
+  app.listen(port, () => {
+    console.log(`La aplicación esta ejecutando en el puerto:  ${port}!`);
+  });
