83b4dd5cdea8206fd015a5b0bb0069d67e70114b
Tu joel jimenez <tonyjoe1110@hotmail.com>
2019-06-18 20:15:51 -0500

    server

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const bodyP = require('body-parser');
 const express = require('express');
 
 const app = express();
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 
 app.set('view engine', 'pug');
